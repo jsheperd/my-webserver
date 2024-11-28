e86d2ab7a03694ee7352642482e68454cd74a8bc
jjuhasz <janos.juhasz@cyansecurity.com>
2024-11-28 10:39:37 +0100

    The app

---
+++ custom.go
@@ -0,0 +1,11 @@
+package caddy_custom
+
+import (
+    "github.com/caddyserver/caddy/v2"
+    // Include other Caddy plugins or modules here.
+)
+
+func init() {
+    // Register modules or custom behaviors here.
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yourusername/my-webserver
+
+go 1.23.3
